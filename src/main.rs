61a7095acea6ef9b465423cdb900e4ab8597186d
Max Jöhnk <maxjoehnk@gmail.com>
2024-08-13 14:01:00 +0200

    perf: run one thread per mapping

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use std::collections::HashMap;
 use sacn_unofficial::packet::ACN_SDT_MULTICAST_PORT;
 use sacn_unofficial::receive::SacnReceiver;
 use serde::Deserialize;
 use std::fs;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
-use indexmap::IndexMap;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Mapping {
@@ -31,57 +31,71 @@ fn main() -> color_eyre::Result<()> {
     let file = fs::read_to_string("bindings.toml")?;
     let config: Config = toml::from_str(&file)?;
 
-    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT);
+    tracing::info!("Translating universes: {:#?}", config);
 
-    let mut dmx_receiver = SacnReceiver::with_ip(addr, None)
-        .map_err(|err| color_eyre::eyre::eyre!("Unable to create sACN Receiver: {err:?}"))?;
+    let mut threads = Vec::with_capacity(config.mappings.len());
 
-    let universes = config.mappings.iter().map(|mapping| mapping.input_universes.iter().copied()).flatten().collect::<Vec<_>>();
-    dmx_receiver.listen_universes(&universes)
-        .map_err(|err| color_eyre::eyre::eyre!("Unable to listen to universes: {err:?}"))?;
+    for mapping in config.mappings.into_iter() {
+        let input_universes  = mapping.input_universes.iter().map(|u| u.to_string()).collect::<Vec<_>>().join("-");
+        let handle = std::thread::Builder::new()
+            .name(format!("universes-{input_universes}"))
+            .spawn(move || {
+                let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), ACN_SDT_MULTICAST_PORT);
 
-    let mappings = config.mappings.into_iter().flat_map(|mapping| {
-        mapping.input_universes.into_iter()
-            .zip(mapping.output_universes.into_iter())
-            .map(move |(input, output)| (input, OutputConfig {
-                universe: output,
-                address: mapping.address,
-            }))
-    }).collect::<IndexMap<u16, _>>();
+                let mut dmx_receiver = SacnReceiver::with_ip(addr, None)
+                    // .map_err(|err| color_eyre::eyre::eyre!("Unable to create sACN Receiver: {err:?}"))
+                    .unwrap();
 
-    tracing::info!("Translating universes: {:#?}", mappings);
+                dmx_receiver.listen_universes(&mapping.input_universes)
+                    .unwrap();
+                    // .map_err(|err| color_eyre::eyre::eyre!("Unable to listen to universes: {err:?}"))?;
 
-    let output_socket = UdpSocket::bind(("0.0.0.0", 0))?;
+                let output_socket = UdpSocket::bind(("0.0.0.0", 0)).unwrap();
 
-    loop {
-        match dmx_receiver.recv(None) {
-            Ok(packet) => {
-                for dmx in packet {
-                    let mapping = match mappings.get(&dmx.universe) {
-                        Some(mapping) => mapping,
-                        None => continue,
-                    };
-                    if let Err(err) = send(&output_socket, mapping, dmx.values) {
-                        tracing::error!("Unable to send to artnet server {err:?}");
+                let mappings = mapping.input_universes.into_iter()
+                    .zip(mapping.output_universes)
+                    .collect::<HashMap<_, _>>();
+
+                let artnet_addr = SocketAddr::new(mapping.address, 6454);
+
+                loop {
+                    match dmx_receiver.recv(None) {
+                        Ok(packet) => {
+                            for dmx in packet {
+                                let universe = match mappings.get(&dmx.universe) {
+                                    Some(mapping) => mapping,
+                                    None => continue,
+                                };
+                                if let Err(err) = send(&output_socket, &artnet_addr, *universe, dmx.values) {
+                                    tracing::error!("Unable to send to artnet server {err:?}");
+                                }
+                            }
+                        }
+                        Err(err) => {
+                            tracing::error!("Error receiving packet: {err:?}");
+                        }
                     }
                 }
-            }
-            Err(err) => {
-                tracing::error!("Error receiving packet: {err:?}");
-            }
-        }
+            })?;
+        threads.push(handle);
     }
+
+    for handle in threads.into_iter() {
+        handle.join().unwrap();
+    }
+
+    Ok(())
 }
 
-fn send(output_socket: &UdpSocket, mapping: &OutputConfig, data: Vec<u8>) -> color_eyre::Result<()> {
+fn send(output_socket: &UdpSocket, address: &SocketAddr, universe: u16, data: Vec<u8>) -> color_eyre::Result<()> {
     let msg = artnet_protocol::Output {
-        port_address: artnet_protocol::PortAddress::try_from(mapping.universe)?,
+        port_address: artnet_protocol::PortAddress::try_from(universe)?,
         data: data.into_iter().take(512).collect::<Vec<_>>().into(),
         ..artnet_protocol::Output::default()
     };
     let msg = artnet_protocol::ArtCommand::Output(msg).write_to_buffer()?;
 
-    output_socket.send_to(&msg, SocketAddr::new(mapping.address, 6454))?;
+    output_socket.send_to(&msg, address)?;
 
     Ok(())
 }
